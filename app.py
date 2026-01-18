477ecfdd62a4077e89ecec2d02b9266f4769b79e
Vansh Pandey <pandeyvansh18@gmail.com>
2026-01-18 14:35:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,60 @@
 import streamlit as st
-import pickle
+import pandas as pd
+import numpy as np
+import faiss
 import requests
 
-tmdb_api_key = st.secrets["TMDB_API_KEY"]
+TMDB_API_KEY = st.secrets['TMDB_API_KEY']
+
+# ---- LOAD DATA ----
+@st.cache_resource
+def load_data():
+    df = pd.read_pickle("movies.pkl")
+    embeddings_np = np.load("embeddings.npy")
+    index = faiss.read_index("movies.index")
+    return df, embeddings_np, index
+
+df, embeddings_np, index = load_data()
+
+# ---- RECOMMEND FUNCTION ----
+def recommend_by_index(movie_idx, k=5):
+    query_emb = embeddings_np[movie_idx].reshape(1, -1)
+    distances, indices = index.search(query_emb, k+1)   # +1 = skip itself
+    return indices[0][1:]  # skip the first one (same movie)
 
 def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={tmdb_api_key}".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}&language=en-US"
+    response = requests.get(url).json()
+    
+    if "poster_path" in response and response['poster_path'] is not None:
+        return "https://image.tmdb.org/t/p/w500" + response['poster_path']
+    else:
+        return None
+    
+# ---- STREAMLIT UI ----
+st.title("🎬 Movie Recommender System")
 
-def recommend(movie):
-    index = df[df['title'] == movie].index[0]
-    distance = sorted(list(enumerate(top_k[index])), reverse=True, key=lambda x: x[1])
+selected_movie = st.selectbox(
+    "Select a movie:",
+    df['title'].values
+)
 
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distance[1:6]:
-        # fetch the movie poster
-        movie_id = df.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(df.iloc[i[0]].title)
-
-    return recommended_movie_names,recommended_movie_posters
-
-
-
-df = pickle.load(open('data.pkl','rb'))
-top_k = pickle.load(open("similar_k.pkl", "rb"))
-
-st.title("MOVIE RECOMMENDATION SYSTEM")
-st.write(":red[.. vansh pandey]")
-selected_name = st.selectbox("Enter ur movie name"
-                    ,df['title'])
-
-if st.button(":blue[Recommend] :sunglasses:"):
-
-    recommended_movie_names,recommended_movie_posters = recommend(selected_name)
-    cols = st.columns(5)
-    for i, col in enumerate(cols):
-        with col:
-            st.text(recommended_movie_names[i])
-
-            st.image(recommended_movie_posters[i])
+if st.button("Recommend"):
+    movie_idx = df[df['title'] == selected_movie].index[0]
+    similar_indices = recommend_by_index(movie_idx, k=5)
+    
+    st.write(f"### Movies similar to **{selected_movie}**:")
+    
+    for idx in similar_indices:
+        title = df.loc[idx, 'title']
+        movie_id = df.loc[idx, 'movie_id']
+        poster = fetch_poster(movie_id)
+    
+        col1, col2 = st.columns([1,3])
+        if poster:
+            col1.image(poster, use_column_width=True)
+        else:
+            col1.write("No poster")
+        
+        col2.write(f"**{title}**")
 
